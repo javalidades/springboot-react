c814716e43ad494ccad39feccd9e5628de09e0b7
Welyab Paula <welyab.exe@gmail.com>
2019-04-19 11:03:47 -0300

    initial commit

---
+++ src/main/java/br/com/javalidades/misc/tutorials/springbootreact/SpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package br.com.javalidades.misc.tutorials.springbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javalidades/misc/tutorials/springbootreact/SpringbootReactApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.javalidades.misc.tutorials.springbootreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootReactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
